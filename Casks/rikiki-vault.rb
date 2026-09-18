2efed2263df38789e539096779b8ead692139ea9
Joao Costa <costajlmpp@gmail.com>
2026-09-18 01:45:39 +0100

    Add rikiki-vault cask

---
+++ Casks/rikiki-vault.rb
@@ -0,0 +1,12 @@
+cask "rikiki-vault" do
+  version "1.0.4"
+  sha256 "04b3881b261d32f884f24265a1f69fcd83944dfb8b5b78d55cc7585b950a44aa"
+
+  url "https://github.com/jlmc/rikiki-vault/releases/download/1.0.4/rikiki-vault-gui-1.0.4.dmg"
+
+  name "Rikiki Vault"
+  desc "Secure personal vault"
+  homepage "https://github.com/jlmc/rikiki-vault"
+
+  app "Rikiki Vault.app"
+end
