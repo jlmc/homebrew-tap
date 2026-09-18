b457ee5e3cf823db30e450bdd2229da4a2efa709
Joao Costa <costajlmpp@gmail.com>
2026-09-18 02:00:47 +0100

    Rename cask from rikiki-vault to rikiki-vault-gui

--- Casks/rikiki-vault-gui.rb
+++ Casks/rikiki-vault-gui.rb
@@ -1,10 +1,10 @@
-cask "rikiki-vault" do
+cask "rikiki-vault-gui" do
   version "1.0.4"
   sha256 "04b3881b261d32f884f24265a1f69fcd83944dfb8b5b78d55cc7585b950a44aa"
 
   url "https://github.com/jlmc/rikiki-vault/releases/download/1.0.4/rikiki-vault-gui-1.0.4.dmg"
 
-  name "Rikiki Vault"
+  name "rikiki-vault-gui"
   desc "Secure personal vault"
   homepage "https://github.com/jlmc/rikiki-vault"
 
